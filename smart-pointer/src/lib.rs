ed769d84350a61f6e504f4af7d49e7e4f461e701
toshinori.sato <tyahhahha@gmail.com>
2023-11-15 22:52:54 +0900

    impl List

--- smart-pointer/src/lib.rs
+++ smart-pointer/src/lib.rs
@@ -1,4 +1,21 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
 pub fn smart_pointer_main() {
     let b = Box::new(5);
     println!("Box: {}", b);
+
+    let list = Cons(
+        1,
+        Box::new(Cons(2,
+            Box::new(Nil)
+        ))
+    );
+
+    println!("list: {:?}", list);
 }
