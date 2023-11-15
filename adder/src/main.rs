77f1bcf84dcc6cf107d21ace9a24848effbac645
toshinori.sato <tyahhahha@gmail.com>
2023-11-15 21:49:31 +0900

    using box

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,7 +1,10 @@
 extern crate add_one;
 extern crate add_two;
+extern crate smart_pointer;
 
 fn main() {
     println!("4 + 1 = {}", add_one::add_one(4));
     println!("6 + 2 = {}", add_two::add_two(6));
+
+    smart_pointer::smart_pointer_main();
 }
---
+++ smart-pointer/src/lib.rs
@@ -0,0 +1,4 @@
+pub fn smart_pointer_main() {
+    let b = Box::new(5);
+    println!("Box: {}", b);
+}
