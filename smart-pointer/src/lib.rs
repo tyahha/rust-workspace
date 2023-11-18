80a1f872dec44de6e79f73f59759822aa830b34e
toshinori.sato <tyahhahha@gmail.com>
2023-11-18 22:04:45 +0900

    Using reference count pointer

--- smart-pointer/src/lib.rs
+++ smart-pointer/src/lib.rs
@@ -1,10 +1,11 @@
 #[derive(Debug)]
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use std::ops::Deref;
+use std::rc::Rc;
 use List::{Cons, Nil};
 
 struct MyBox<T>(T);
@@ -42,8 +43,8 @@ pub fn smart_pointer_main() {
 
     let list = Cons(
         1,
-        Box::new(Cons(2,
-            Box::new(Nil)
+        Rc::new(Cons(2,
+            Rc::new(Nil)
         ))
     );
 
@@ -75,4 +76,19 @@ pub fn smart_pointer_main() {
     let c = CustomSmartPointer { data: String::from("smart pointer c")};
     let d = CustomSmartPointer { data: String::from("smart pointer d")};
     println!("created CustomSmartPointer");
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("counter after creating a: {}", Rc::strong_count(&a));
+    println!("a: {:?}", a);
+
+    let b = Cons(3, Rc::clone(&a));
+    println!("counter after creating b: {}", Rc::strong_count(&a));
+    println!("b: {:?}", b);
+
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("counter after creating c: {}", Rc::strong_count(&a));
+        println!("c: {:?}", c);
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
