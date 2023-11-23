8b4485b648746e562d852c29186b60a6b204ee6d
toshinori.sato <tyahhahha@gmail.com>
2023-11-23 21:46:26 +0900

    thread spawn

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,10 +1,12 @@
 extern crate add_one;
 extern crate add_two;
 extern crate smart_pointer;
+extern crate fearless_concurrency;
 
 fn main() {
     println!("4 + 1 = {}", add_one::add_one(4));
     println!("6 + 2 = {}", add_two::add_two(6));
 
     smart_pointer::smart_pointer_main();
+    fearless_concurrency::fearless_concurrency_main();
 }
