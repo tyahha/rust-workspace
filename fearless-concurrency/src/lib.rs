cbfe6b50dc36df806bd09c59f66fd7644b348396
toshinori.sato <tyahhahha@gmail.com>
2023-11-23 21:57:54 +0900

    capture env into thread

--- fearless-concurrency/src/lib.rs
+++ fearless-concurrency/src/lib.rs
@@ -13,10 +13,16 @@ fn thread_spawn_sample() {
         }
     });
 
+    let v = vec![1,2,3];
+    let handle2 = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
     for i in 1..5 {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
 
     handle.join().unwrap();
+    handle2.join().unwrap();
 }
