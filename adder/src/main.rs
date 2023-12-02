b7c5a0b87720b2060f05e89653f26fe91d70f2f5
toshinori.sato <tyahhahha@gmail.com>
2023-12-02 23:52:34 +0900

    oop sample

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -3,6 +3,7 @@ extern crate add_two;
 extern crate smart_pointer;
 extern crate fearless_concurrency;
 extern crate oop_in_rust;
+extern crate blog;
 
 fn main() {
     println!("4 + 1 = {}", add_one::add_one(4));
@@ -11,4 +12,15 @@ fn main() {
     smart_pointer::smart_pointer_main();
     fearless_concurrency::fearless_concurrency_main();
     oop_in_rust::oop_in_rust_main();
+
+    let mut post = Pose::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    asset_eq!("I ate a salad for lunch today", post.content());
 }
