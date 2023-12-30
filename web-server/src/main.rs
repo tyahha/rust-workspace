604920a7d6bad232de63535ec2697216c44ec050
toshinori sato <tyahhahha@gmail.com>
2023-12-30 23:02:59 +0900

    handle connection

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,10 +1,20 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established!");
+
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+
+    stream.read(&mut buf).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buf[..]));
 }
